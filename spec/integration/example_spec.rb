14efe1a01099dc243c28238dd21c865725229fb3
mo khan <mo@mokhan.ca>
2013-05-22 22:32:41 -0600

    split spec between fetching records and inserting records

--- spec/integration/example_spec.rb
+++ spec/integration/example_spec.rb
@@ -15,9 +15,6 @@ describe "orm" do
     end
 
     configuration.add(MovieMapping.new)
-    session.begin_transaction do |session|
-      session.save(Movie.new(:name => 'monsters inc'))
-    end
   end
 
   after :each do
@@ -25,6 +22,10 @@ describe "orm" do
   end
 
   context "when fetching all items" do
+    before :each do
+      connection[:movies].insert(:name => 'monsters inc')
+    end
+
     let(:results) { session.find_all Movie }
 
     it "should return the correct number of movies" do
@@ -37,6 +38,29 @@ describe "orm" do
 
     it "should return instances of the target type" do
       results.first.should be_instance_of(Movie)
+    end
+  end
+
+  context "when inserting a new record" do
+    before :each do
+      session.begin_transaction do |session|
+        session.save(Movie.new(:name => 'oop'))
+      end
+    end
+
+    let(:results) { connection[:movies].all }
+
+    it "should insert the correct number of records" do
+      results.count.should == 1
+    end
+
+    it "should insert the record with the a new id" do
+      results.first[:id].should_not == -1
+      results.first[:id].should > 0
+    end
+
+    it "should insert the name" do
+      results.first[:name].should == 'oop'
     end
   end
 
